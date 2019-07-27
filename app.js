5c7881d93c40d92e209c449c1d4dd47d087be724
Ed Chebli <chah762002@yahoo.fr>
2019-07-27 01:24:15 -0700

    app.js, camp and user routes

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const campRouter = require('./routes/campRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 3) ROUTES
+app.use('/api/camps', campRouter);
+app.use('/api/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+
+module.exports = app;
